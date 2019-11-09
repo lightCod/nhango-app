34a811b842b45997c69af99b817d9c6940fc806b
Antonio Cardoso <buda.cardoso@gmail.com>
2019-11-09 14:00:24 -0800

    Model & Crontrollers
    Created model and empty controllers

---
+++ app/Http/Controllers/FuelRequestController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\FuelRequest;
+use Illuminate\Http\Request;
+
+class FuelRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\FuelRequest  $fuelRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FuelRequest $fuelRequest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\FuelRequest  $fuelRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FuelRequest $fuelRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\FuelRequest  $fuelRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FuelRequest $fuelRequest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\FuelRequest  $fuelRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FuelRequest $fuelRequest)
+    {
+        //
+    }
+}
