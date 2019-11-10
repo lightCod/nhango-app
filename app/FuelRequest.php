20be983dfdef504e67cac80c51a2ebcf55272a92
Antonio Santos Antonio Cardoso <buda.cardoso@gmail.com>
2019-11-10 11:48:55 +0200

    Migartion&Relationship
    Adicionado os migrations e realacoes

--- app/FuelRequest.php
+++ app/FuelRequest.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class FuelRequest extends Model
 {
-    //
+    public function users(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function locations(){
+        return $this->belongsTo('App\Location');
+    }
+
+    public function vehicles(){
+        return$this->belongsTo('App\Vehicles');
+    }
 }
--- app/Vehicles.php
+++ app/Vehicles.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vehicles extends Model
 {
-    //
+    public function fuelRequests(){
+        return $this->hasMany('App\FuelRequest');
+    }
 }
